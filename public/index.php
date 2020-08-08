494fef30211dcb5c4714aa59e198e3e473afdb3b
ko2 <ko22009@yandex.ru>
2020-08-08 14:52:39 +0500

    Add dev show errors; Replace creating app.

--- public/index.php
+++ public/index.php
@@ -3,26 +3,28 @@
 /** @var ContainerInterface $container */
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
-use Slim\App;
 use Slim\Interfaces\CallableResolverInterface;
 use Slim\Interfaces\RouteCollectorInterface;
 use Slim\Interfaces\RouteResolverInterface;
 use Slim\Middleware\ErrorMiddleware;
 use Slim\Middleware\RoutingMiddleware;
 use Slim\Psr7\Factory\ServerRequestFactory;
+use Slim\Factory\AppFactory;
 
 $container = require dirname(__DIR__) . '/bootstrap.php';
 $request = ServerRequestFactory::createFromGlobals();
 
-Slim\Factory\AppFactory::create();
+AppFactory::setContainer($container);
 
-$app = new App(
+$app = AppFactory::create(
     $container->get(ResponseFactoryInterface::class),
     $container,
     $container->get(CallableResolverInterface::class),
     $container->get(RouteCollectorInterface::class),
     $container->get(RouteResolverInterface::class)
 );
+
+if($env == 'dev') $app->addErrorMiddleware(true, false, false);
 
 $app->add($container->get(RoutingMiddleware::class));
 $app->add($container->get(ErrorMiddleware::class));
